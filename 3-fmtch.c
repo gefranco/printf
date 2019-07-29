292d1b913cfb7562628c2484d3b2053c60e6dd4c
gomba66 <danielalejo66@hotmail.com>
2019-07-29 00:49:11 +0000

    Task 0: [fix] Fixed Betty Style - 3-fmtch.c file

--- 3-fmtch.c
+++ 3-fmtch.c
@@ -1,9 +1,15 @@
 #include <stdarg.h>
 #include <unistd.h>
+/**
+ * prt_char - This is a function that process the string that is "c" type.
+ * @valist: Is the name of the va_list.
+ * Return: cc.
+ */
 int prt_char(va_list valist)
 {
-        int cc = 0;
-        int *c = va_arg(valist,int);    
-        cc += write(STDOUT_FILENO, &c, 1);
-        return (cc);    
+	int cc = 0;
+	int *c = va_arg(valist, int);
+
+	cc += write(STDOUT_FILENO, &c, 1);
+	return (cc);
 }
