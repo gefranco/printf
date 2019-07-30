0ce2c6fa8fa8b8d9c3144ece5905ed2cbe027011
German Franco F <933@holbertonschool.com>
2019-07-30 01:23:58 +0000

    task 1 : add functionality to manage %d 1-fmtmng.c

--- 1-fmtmng.c
+++ 1-fmtmng.c
@@ -5,7 +5,8 @@ int mngfmt(int c, va_list valist)
 {
 	prt_f prto[] = {
 		{99, prt_char},
-		{115, prt_str}
+		{115, prt_str},
+		{100, prt_int}
 	};
 	
 	if (c == 99)
@@ -16,6 +17,8 @@ int mngfmt(int c, va_list valist)
 	{
 		return (prto[1].f(valist));
 	}
+	else if (c == 100)
+		return (prto[2].f(valist));
 	else 
 	{
 		return(0);
