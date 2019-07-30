23bb7b7033e58709d4a03ed49fbdba6143f94f50
gomba66 <danielalejo66@hotmail.com>
2019-07-30 02:54:52 +0000

    Task 1: [fix] Fixed the betty style - 4-fmtint.c file

--- 4-fmtint.c
+++ 4-fmtint.c
@@ -3,56 +3,71 @@
 #include <unistd.h>
 #include <stdlib.h>
 char *itoch(int number);
-int cntdtsn (int number);
+int cntdtsn(int number);
+/**
+ * prt_int - This is a function that prints an integer
+ * @valist: Is the name of va_list
+ * Return: cc - 1.
+ */
 int prt_int(va_list valist)
-{	
-
+{
 	int cc = 0;
-        int number = va_arg(valist,int);
+	int number = va_arg(valist, int);
 	int i;
 	char *digits;
 	int cnt = cntdtsn(number);
+
 	digits = itoch(number);
 
-
-	for(i = 0; i <= cnt; i++)
-        {
+	for (i = 0; i <= cnt; i++)
+	{
 		cc += write(STDOUT_FILENO, digits++, 1);
-        }
-	return (cc - 1);    
+	}
+	return (cc - 1);
 }
-int cntdtsn (int number)
+/**
+ * cntdtsn - This is a function that count the digits of a number.
+ * @number: Is the number that join in the function.
+ * Return: count.
+ */
+int cntdtsn(int number)
 {
 	int count = 1;
+
 	number = number / 10;
-        while (number > 0)
-        {
-                number = number / 10;
+	while (number > 0)
+	{
+		number = number / 10;
 		count++;
-        }
-        return (count);
+	}
+	return (count);
 }
+/**
+ * itoch - This is a function for change a integer to a char.
+ * @number: This is the number that changed to a char.
+ * Return: a char.
+ */
 char *itoch(int number)
 {
 	char nn;
 	int number2 = number;
 	int count = 1;
-	int i = 0;	
+	int i = 0;
 	char *digits;
-	int j = 0;	
-	
+	int j = 0;
+
 	count = cntdtsn(number2);
 	i = count;
-	number2 = number;	
+	number2 = number;
 	digits = malloc(count * sizeof(char));
-	while((number2 = number2 / 10) > 0)
+	while ((number2 = number2 / 10) > 0)
 	{
 		number = number % 10;
-		nn = number  + '0';		
-		number = number2 % 10;	
+		nn = number  + '0';
+		number = number2 % 10;
 		digits[i] = nn;
 		i--;
-		j++;	
+		j++;
 	}
 	number = number % 10;
 	nn = number + '0';
