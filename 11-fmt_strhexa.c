e460efa08ea7af2cd6835b31f571fc1d29750b89
gomba66 <danielalejo66@hotmail.com>
2019-07-30 20:19:17 +0000

    [General] Added a new function - 11-fmt_strhexa.c file

---
+++ 11-fmt_strhexa.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * prt_strhexa - This is a function for print non printable characters in hexa
+ * @valist: Is the name of the va_list.
+ * Return: 0.
+ */
+int prt_strhexa(va_list valist)
+{
+	(void) valist;
+	return (0);
+}
